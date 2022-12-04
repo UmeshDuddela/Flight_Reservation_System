fb7b77e032e8c93ffbb519a75b912a529cd3fba2
sreeram1599 <65607618+sreeram1599@users.noreply.github.com>
2022-12-03 23:10:58 -0600

    Update FlightTicketReservationSystem.java

--- FlightTicketReservationSystem.java
+++ FlightTicketReservationSystem.java
@@ -1,25 +1,25 @@
 } else {
-								System.out.println("\nSelected Flight :");
+								System.out.println("\nThe Selected Flight is :");
 								flt[tkt].getroute();
 								arr = seatAvailability();
 								if (arr[2] == 1) {
-									if (arr[1] == 0) {
+									if (arr[1] == n) {
 										section = "Economy Class";
 									} else if (arr[1] == 1) {
 										section = "First Class";
 									}
-									System.out.println("\nPress 1 to confirm selected flight else press 0 :");
-									cnf = sc.nextInt();
-									if (cnf == 1) {
-										System.out.println("press 1 to make payment and book ticket else press 0 : ");
-										pmt = sc.nextInt();
-										if (pmt == 1) {
-											System.out.println("\nTicket booked sucessfully!!\n");
-											System.out.println("\n----------------Your Ticket Details--------------");
-											tck[id].setticket(id, date, section, arr[0], tkt);
+									System.out.println("\nPress 1 to confirm the desired selected flight else press 0 :");
+									cnrf = sc.nextInt();
+									if (cnrf == 1) {
+										System.out.println("press 1 to make payment and book tickets else press 0 : ");
+										pamt = sc.nextInt();
+										if (pamt == 1) {
+											System.out.println("\n###Your ticket is booked sucessfully###\n");
+											System.out.println("\n**************Your Ticket Details**************");
+											tckt[id].setticket(id, date, section, arr[0], tkt);
 											if (arr[1] == 1)
 												flt[id].fare *= 2;
-											tck[id].getticket();
+											tckt[id].getticket();
 											flt[tkt].getroute();
 											id++;
 										}
@@ -29,7 +29,7 @@
 						}
 
 						else if (depr == arvl)
-							System.out.println("\n\nError! You have entered same Departure and Arrival city!!");
+							System.out.println("\n\nError! The entered Departure and Arrival city are same!!");
 						else
 							System.out.println("\nError! Please enter a Valid City Choice!!");
 						break;
@@ -39,16 +39,16 @@
 						int del = sc.nextInt();
 						flag = 0;
 						for (int i = 0; i < 10; i++) {
-							if (del == tck[i].ticket) {
-								System.out.println("\n----------------Your Ticket Details--------------");
-								tck[del].getticket();
-								flt[tck[del].code].getroute();
+							if (del == tckt[i].ticket) {
+								System.out.println("\n################Your Ticket Details##############");
+								tckt[del].getticket();
+								flt[tckt[del].code].getroute();
 								System.out.println("Press 1 to confirm Cancellation of Your Ticket else press 0 : ");
 								int d = sc.nextInt();
 								flag = 1;
 								if (d == 1) {
 
-									tck[del].ticket = 111;
+									tckt[del].ticket = 111;
 									System.out.println("\nYour Ticket has been cancelled Successfully.");
 									System.out.println("You will get Refund Amount within two days.\n\n");
 								}
